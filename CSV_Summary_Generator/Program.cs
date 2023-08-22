54db76d7c076959038086377acc2902677ac0307
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-08-22 19:16:50 -0300

    Gerador de registro de pedido final, utilizando Path

---
+++ CSV_Summary_Generator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using CSV_Summary_Generator.Entities;
+
+namespace CSV_Summary_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.cvs";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + ", $" + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Write("Error: " + e.Message);
+            }
+        }
+    }
+}
